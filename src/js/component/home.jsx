7dd7b953e4bda51aa21a7828ba19e928bcb8bf3a
kjatkinson <149319433+kjatkinson@users.noreply.github.com>
2024-03-06 00:25:28 +0000

    added trashicons n todo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import rigoImage from '../../img/rigo-baby.jpg';
+
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>To Do List</h1>
+			<ul>
+				<li><input type="text" placeholder="What do you need?"></input>   </li>
+				<li>Walk the dog <i class="fas fa-trash"></i> </li>
+				<li>Pay taxes <i class="fas fa-trash"></i>  </li>
+				<li>Go on vacation <i class="fas fa-trash"></i>  </li>
+				<div> 23 tasks  </div>
+			</ul>
 		</div>
+		
 	);
 };
 
