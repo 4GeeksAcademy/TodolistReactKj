e5236d565bd343a43a75e4de38e63faa9439c14d
kjatkinson <149319433+kjatkinson@users.noreply.github.com>
2024-03-06 01:12:48 +0000

    not comp

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from '../../img/rigo-baby.jpg';
-
+import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="container">
-			<h1>To Do List</h1>
-			<ul>
-				<li><input type="text" placeholder="What do you need?"></input>   </li>
-				<li>Walk the dog <i class="fas fa-trash"></i> </li>
-				<li>Pay taxes <i class="fas fa-trash"></i>  </li>
-				<li>Go on vacation <i class="fas fa-trash"></i>  </li>
-				<div> 23 tasks  </div>
-			</ul>
-		</div>
-		
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState ([]); 
+  return (
+    <div className="container">
+      <h1>My Todos</h1>
+      <ul>
+        <li>
+          <input>
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            handleKeyPres={(e) => {
+              if (e.keyCode === "Enter"){
+          setTodos (todos.concat([inputValue]));
+          setInputValue ("") }}
+        }
+            placeholder="What do you need?" 
+        </li>
+        <li>
+          Walk the dog <i class="fas fa-trash"></i>{" "}
+        </li>
+        <li>
+          Pay taxes <i class="fas fa-trash"></i>{" "}
+        </li>
+        <li>
+          Go on vacation <i class="fas fa-trash"></i>{" "}
+        </li>
+        <div> 23 tasks </div>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
